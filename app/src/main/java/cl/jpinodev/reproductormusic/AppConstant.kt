4aa3a4550a873fec49d8a7238a464fa602a40f1c
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2024-05-09 15:06:03 -0400

    Implementacion modo land con lyrics de la cancion en reproduccion.

--- app/src/main/java/cl/jpinodev/reproductormusic/AppConstant.kt
+++ app/src/main/java/cl/jpinodev/reproductormusic/AppConstant.kt
@@ -3,7 +3,8 @@ package cl.jpinodev.reproductormusic
 data class Song (
     val title: String,
     val audioResId: Int,
-    val imageResId: Int
+     val imageResId: Int,
+    val lyricsResId: Int
 ){}
 
 class AppConstant {
@@ -13,9 +14,9 @@ class AppConstant {
         const val MEDIAPLAYER_POSITION = "mpPosition"
 
         val Songs = listOf(
-            Song("Ride the Lightning", R.raw.m_ridethelightning, R.drawable.ridethelightning_metallica),
-            Song("Heaven and Hell", R.raw.heavenandhell_blacksabbath, R.drawable.heaven_and_hell_blacksabbath),
-            Song("Painkiller", R.raw.painkiller_judaspriest, R.drawable.painkiller_judaspriest)
+            Song("Painkiller", R.raw.painkiller_judaspriest, R.drawable.painkiller_judaspriest, R.string.painkiller),
+            Song("Ride the Lightning", R.raw.m_ridethelightning, R.drawable.ridethelightning_metallica, R.string.ride_the_lightning),
+            Song("Heaven and Hell", R.raw.heavenandhell_blacksabbath, R.drawable.heaven_and_hell_blacksabbath, R.string.heaven_and_hell),
         )
     }
 }
